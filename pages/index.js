bd4128e3ee9c09004ac0499b8236c335ca3770fd
erlangga092 <erlanggaangga092@gmail.com>
2020-07-05 00:52:21 +0700

    meta viewport

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home(props) {
       <Head>
         <meta charSet="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"/>
         <meta name="AdsBot-Google" content="noindex"/>
         <meta name="description" content="Home page of erlangga blog"/>
         <meta property="og:type" content="website"/>
