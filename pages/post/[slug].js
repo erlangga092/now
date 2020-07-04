6fd36b5ce983ef900f22689ac5efea1627e7b5fa
erlangga092 <erlanggaangga092@gmail.com>
2020-07-05 01:00:27 +0700

    meta index

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -24,7 +24,7 @@ export default function Post({content, frontMatter, slug}) {
 			<meta charSet="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"/>
-        <meta name="AdsBot-Google" content="noindex"/>
+        <meta name="robots" content="noindex" />
         <meta name="description" content="Post page of erlangga blog"/>
         <meta property="og:type" content="website"/>
         <meta property="og:site_name" content="{slug} - Erlangga"/>
